00d461b4f5797e82c8c7205a93403b7d15ff1951
Vladislav Saraykin <vlad.saraykin@mail.ru>
2018-11-15 21:59:44 +0300

    add help method for work with input data

--- form/index.php
+++ form/index.php
@@ -7,16 +7,19 @@
         <meta charset="UTF-8">
     </head>
     <body>
-    <form action="get.php" method="POST" accept-charset="UTF-8">
+    <form action="post.php" method="POST" accept-charset="UTF-8">
         <input type="text" name="name" placeholder="Введите Ваше имя"/><br>
-        <textarea name="msg" id="" cols="30" rows="10" placeholder="Сообщение"></textarea><br>
         <span>Ваш пол:</span><br>
-        <input type="radio" id="man" name="sex" value="male"/>
-        <label for="man">Мужчина</label><br>
-        <input type="radio" id="women" name="sex" value="female"/>
-        <label for="women">Женщина</label><br>
-        <span></span>
-        <input type="checkbox" name="checkbox"/><br>
+        <input type="radio" name="sex" value="male"/>Мужчина<br>
+        <input type="radio" name="sex" value="female"/>Женщина<br>
+        <span>Что Вам понравилось?</span><br/>
+        <input type="checkbox" name="resultPoll[]" value="service"/>Обслуживание<br>
+        <input type="checkbox" name="resultPoll[]" value="staff"/>Персонал<br>
+        <input type="checkbox" name="resultPoll[]" value="price"/>Цены<br>
+        <input type="checkbox" name="resultPoll[]" value="program"/>Программа<br>
+        <input type="checkbox" name="resultPoll[]" value="atmosphere"/>Обстановка<br>
+        <input type="hidden" name="hidden">
+        <textarea name="msg" id="" cols="30" rows="10" placeholder="Ваш вариант"></textarea><br>
         <input type="submit">
     </form>
     </body>
---
+++ form/post.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vlada
+ * Date: 13.11.2018
+ * Time: 16:11
+ */
+
+$name = $_POST['name'];
+$msg = $_POST['msg'];
+$sex = $_POST['sex'];
+$resultOfPoll = $_POST['resultPoll'];
+$sender = $_SERVER['SERVER_NAME'];
+$sex = getRussianValue($sex);
+$resultOfPollRus = getRussianResultOfPoll($resultOfPoll);
+$valuesOfPollStr = getElementOfArrStr($resultOfPollRus);
+
+mail("vlad.saraykin@mail.ru", "Вам пришло письмо с сайта $sender",
+   "Отзыв поситителя:($sex) $name \n \n$valuesOfPollStr Сообщение: $msg");
+
+function getRussianValue($arg){
+    switch ($arg) {
+        case 'service':
+            return 'обслуживание';
+        case 'staff':
+            return 'персонал';
+        case 'price':
+            return 'цена';
+        case 'program':
+            return 'программа';
+        case 'atmosphere':
+            return 'обстановка';
+        case 'male':
+            return 'Мужчина';
+        case 'female':
+            return 'Женщина';
+    }
+    return 'Undefined value';
+}
+
+function getElementOfArrStr($arr){
+    if (sizeof($arr) != 0){
+        $result = "Клиенту понравилось: \n";
+        foreach ($arr as $item){
+            $result = $result."\t $item\n";
+        }
+        return $result;
+    }else{
+        return "Клиент не выбрал ни один предложенный вариант\n";
+    }
+}
+
+function getRussianResultOfPoll($resultOfPoll){
+    $arr = [];
+    foreach ($resultOfPoll as $item){
+        $arr[] = getRussianValue($item);
+    }
+    return $arr;
+}
